09fbe49ad92028ba5629ddacc118e4f1746c839c
Clement Hoang <clement.hoang24@gmail.com>
2016-09-18 01:04:03 -0400

    Update lat and lng in map state

--- client/components/main/map/index.js
+++ client/components/main/map/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { container } from './styles';
 import Autocomplete from 'react-google-autocomplete';
+import { GoogleMap, Marker, GoogleMapLoader } from "react-google-maps";
 
 export default class MapView extends Component {
   /*eslint-disable */
@@ -9,6 +10,32 @@ export default class MapView extends Component {
     callback(); // this call is important, don't forget it
   }
   /*eslint-enable */
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      lng: -80.5204096,
+      lat: 43.4642578
+    }
+  }
+
+  componentDidMount() {
+    if ('geolocation' in navigator) {
+      this.watchID = navigator.geolocation.watchPosition((position) => {
+        this.setState({
+          lng: position.coords.longitude,
+          lat: position.coords.latitude
+        })
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.clearWatch(this.watchID);
+    }
+  }
 
   render() {
     return <div className={container}>
@@ -22,12 +49,36 @@ export default class MapView extends Component {
         onChange={this.stopPropagation}
         types={['(regions)']}
       />
+      <GoogleMapLoader
+        query={{ libraries: "places,visualization" }}
+        containerElement={
+          <div
+            style={{
+              height: "88vh"
+            }}
+          />
+        }
+        googleMapElement={
+          <GoogleMap
+            ref={(map) => console.log(map)}
+            defaultZoom={16}
+            defaultCenter={{ lat: this.state.lat, lng: this.state.lng }}
+            onClick={this.props.onMapClick}
+          >
+          </GoogleMap>
+        }
+      />
     </div>
   }
 
   onPlaceSelected = (place) => {
     console.log(place.geometry.location.lat());
     console.log(place.geometry.location.lng());
+
+    this.setState({
+      lng: place.geometry.location.lng(),
+      lat: place.geometry.location.lat()
+    });
   }
 
   stopPropagation = (e) => {e.stopPropagation()}
